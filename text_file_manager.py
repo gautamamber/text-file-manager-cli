990ac180c206aa1a2e33d51b68f03e22947146b5
gautamamber <ambergautam1@gmail.com>
2020-03-01 15:18:20 +0530

    feat():add copy, delete, rename cli

--- text_file_manager.py
+++ text_file_manager.py
@@ -43,16 +43,75 @@ def read(args):
         print(file.read())
 
 
+def delete(args):
+    """
+    Get the file name and delete
+    """
+    file_name = args.delete[0]
+    validate_file(file_name)
+    # Delete the file
+    os.remove(file_name)
+    print("File successfully deleted")
+
+
+def copy(args):
+    """
+    Copy file
+    """
+    file_1 = args.copy[0]
+    file_2 = args.copy[1]
+
+    validate_file(file_1)
+    # Validate the file 2
+    if not validate_file_type(file_2):
+        print(INVALID_FILE_TYPE_MESSAGE)
+        exit()
+    with open(file_1, 'r') as f1:
+        with open(file_2, 'w') as f2:
+            f2.write(f1.read())
+    print("File successfully copy")
+
+
+def rename(args):
+    """
+    Rename file
+    """
+    old_filename = args.rename[0]
+    new_filename = args.rename[1]
+    validate_file(old_filename)
+    # validate the new file type
+    if not validate_file_type(new_filename):
+        print(INVALID_FILE_TYPE_MESSAGE)
+        exit()
+    os.rename(old_filename, new_filename)
+    print("File is successfully rename")
+
+
 def main():
     """
     Create parser object
     """
     parser = argparse.ArgumentParser(description="A text file manager CLI")
+    # Read a file
     parser.add_argument('-r', '--read', type=str, nargs=1, metavar="file_name", default=None,
                         help="Open and read a specific file")
+    # Delete a file
+    parser.add_argument('-d', '--delete', type=str, nargs=1, metavar="file_name", default=None, help="Delete a file")
+    # Copy a file
+    parser.add_argument('-c', '--copy', type=str, nargs=2, metavar=('file_1', 'file_2'), help="Copy a file")
+    # rename a file
+    parser.add_argument('-rn', '--rename', type=str, nargs=2, metavar=('file_1', 'file_2'), help="Rename a file")
+
+    # parse the argument from standard output
     args = parser.parse_args()
     if args.read is not None:
         read(args)
+    if args.delete is not None:
+        delete(args)
+    if args.copy is not None:
+        copy(args)
+    if args.rename is not None:
+        rename(args)
 
 
 if __name__ == "__main__":
